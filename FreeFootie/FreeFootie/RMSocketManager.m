39a9807e56840d58e6ccb219a68d553f3539d6f8
Ryan Moniz <ryanmoniz@gmail.com>
2014-03-24 00:37:39 -0600

    Created RMSocketManager singleton class for handling websocket to server. added frameworks and libSocketRocket to project

---
+++ FreeFootie/FreeFootie/RMSocketManager.m
@@ -0,0 +1,13 @@
+//
+//  RMSocketManager.m
+//  FreeFootie
+//
+//  Created by macmoniz on 2014-03-24.
+//  Copyright (c) 2014 ryanmoniz. All rights reserved.
+//
+
+#import "RMSocketManager.h"
+
+@implementation RMSocketManager
+
+@end
